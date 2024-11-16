298400a022b6e7e78cb678cbbca831d97d178684
Eliermoreta <eliermoretaencarnacion@gmail.com>
2024-11-15 22:25:39 -0400

    Add files via upload

---
+++ proyecto8/Components/Models/Visita.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
+public class Visita{
+    public string Nombre {get; set;}
+    public string Apellido {get; set;}
+    public string Telefono {get; set;}
+    public string Correo {get; set;}
+}
